a1930a4b783fe2ada171d123935e19eec25e72c5
Oleg Sereda <sereda1961@gmail.com>
2022-07-25 00:15:02 +0300

    getTime

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println(new Date());
+        long date = new Date().getTime();
+        long mont = new Date().getMonth();
+        System.out.println(date +" "+mont);
+//        int mont = date.getMonth();
     }
 }
