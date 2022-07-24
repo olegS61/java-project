44d72bb5082414439c4379a3c10fa5889e1c24ea
Oleg Sereda <sereda1961@gmail.com>
2022-07-24 23:59:02 +0300

    new Date

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello, привет");
+        System.out.println(new Date());
     }
 }
