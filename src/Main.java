217f1e916a23e8b3ee81be5de6c459ca8e603725
Oleg Sereda <sereda1961@gmail.com>
2022-07-25 07:33:13 +0300

    object Calendar

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println(new Date());
-        long date = new Date().getTime();
-        long mont = new Date().getMonth();
-        System.out.println(date +" "+mont);
+       // System.out.println(new Date());
+        Calendar calendar = Calendar.getInstance();
+        int mont = calendar.get(Calendar.MONTH);
+        int hour = calendar.get(Calendar.HOUR);
+        System.out.println(mont + "  " +hour);
 //        int mont = date.getMonth();
     }
 }
