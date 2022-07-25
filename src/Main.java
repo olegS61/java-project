d688a7aa699cc2f1ed2e252c96efca443f1c4d57
Oleg Sereda <sereda1961@gmail.com>
2022-07-25 19:38:04 +0300

    Date

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -5,7 +7,7 @@ import java.util.GregorianCalendar;
 public class Main {
 
     public static void main(String[] args) {
-       // System.out.println(new Date());
+        System.out.println(new Date());
         Calendar calendar = Calendar.getInstance();
         int mont = calendar.get(Calendar.MONTH);
         int hour = calendar.get(Calendar.HOUR);
@@ -16,5 +18,8 @@ public class Main {
         box.height = 10.2;
         box.width = 11.3;
         System.out.println(box.getVolume());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
+        LocalDateTime tim = LocalDateTime.now();
+        System.out.println(tim.format(formatter));
     }
 }
